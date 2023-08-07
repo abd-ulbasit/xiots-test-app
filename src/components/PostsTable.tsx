015b8ef8ff4e8d26a80bada42a119ee48fef1c1c
abd-ulbasit <basit01035@gmail.com>
2023-08-07 21:18:27 +0500

    fix line clamping and add action placeholders

--- src/components/PostsTable.tsx
+++ src/components/PostsTable.tsx
@@ -13,21 +13,39 @@ export default function PostsTable() {
     // and render the buttons on the bottom that show current page and total pages and when clicked should change the page no. and records on page
     return (
         <div>
-            <table className="table-auto">
+            <table className="table-auto w-4/5">
                 <thead>
                     <tr>
-                        <th className="px-4 py-2">ID</th>
-                        <th className="px-4 py-2">Title</th>
-                        <th className="px-4 py-2">Body</th>
+                        <th className="">ID</th>
+                        <th className="">Title</th>
+                        <th className="">Body</th>
+                        <th>Action</th>
                     </tr>
                 </thead>
                 <tbody>
                     {posts.slice((pagination.active - 1) * (NO_OF_RECORDS_PER_PAGE), (pagination.active) * (NO_OF_RECORDS_PER_PAGE)).map((post) => (
-                        <tr key={post.id}>
-                            <td className="border px-4 py-2">{post.id}</td>
-                            <td className="border px-4 py-2">{post.title}</td>
-                            <td className="border px-4 py-2 line-clamp-1 ">{post.body}</td>
+                        <tr key={post.id} className='border'>
+                            <td className="">{post.id}</td>
+                            <td className="overflow-hidden">
+                                <p className='line-clamp-1'>
+                                    {post.title}
+                                </p>
+                            </td>
+                            <td className="overflow-hidden">
+                                <p className='line-clamp-1'>
+                                    {post.body}
+                                </p>
+                            </td>
+                            <td className="flex">
+                                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                                    Edit
+                                </button>
+                                <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
+                                    Delete
+                                </button>
+                            </td>
                         </tr>
+
                     ))}
                 </tbody>
             </table>
