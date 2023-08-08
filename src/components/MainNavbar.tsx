ee958b4323a9ea3fcbedd2c55fe02b6453e82c44
abd-ulbasit <basit01035@gmail.com>
2023-08-08 20:58:30 +0500

    sytle mainnavbar for sm and lg screens

--- src/components/MainNavbar.tsx
+++ src/components/MainNavbar.tsx
@@ -2,7 +2,8 @@ import { usePostsStore } from '@/store/posts';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import React, { useEffect, useState } from 'react';
-
+import { FaHome, FaMailBulk, FaPhoneVolume } from "react-icons/fa"
+import { FiPhone } from "react-icons/fi"
 interface NavbarProps {
     // Add any props you might need for the navbar
 }
@@ -36,61 +37,90 @@ const MainNavbar: React.FC<NavbarProps> = () => {
     return (
         <section>
 
-            <nav className="bg-gray-800 p-4 flex items-center justify-between">
-                <div className="flex items-center">
-                    <h1 className="text-white text-2xl mr-4">Your Logo</h1>
+            <nav >
+                <div className="h-24 pl-6 pr-10 flex-wrap flex items-center justify-between" >
+
+                    <div className="flex items-center w-1/4">
+                        <h1 className=" text-2xl mr-4 italic font-bold font-sans"> Logo</h1>
+                    </div>
+                    <button
+                        className="text-2xl md:hidden "
+                        onClick={handleToggle}
+                    >
+                        &#9776;
+                    </button>
+                    <div
+                        className={`  md:flex flex-row hidden flex-grow md:items-center  gap-3 justify-between`}
+                    >
+                        <div className='flex-row flex items-center gap-2'>
+                            <div className='text-yellow-300 p-2 border-yellow-300 rounded-full border'>
+                                <FaHome></FaHome>
+                            </div>
+                            <div className='flex flex-col gap-1'>
+                                <div className='text-yellow-300 font-bold'> Address</div>
+                                <div className='text-gray-800 text-sm'> Example England</div>
+                            </div>
+                        </div>
+                        <div className='flex-row flex items-center gap-2'>
+                            <div className='text-yellow-300 p-2 border-yellow-300 rounded-full border'>
+                                <FaMailBulk></FaMailBulk>
+                            </div>
+                            <div className='flex flex-col gap-1'>
+                                <div className='text-yellow-300 font-bold'>Email Us</div>
+                                <div className='text-gray-800 text-sm'> example@booking.com</div>
+                            </div>
+                        </div>
+                        <div className='flex-row flex items-center gap-2 relative '>
+                            <div className='absolute -top-12 pt-10 p-2 bg-gray-800 rounded-b-full '>
+                                <div className='text-white bg-gray-600 p-2 border-white  rounded-full border'>
+                                    <FaPhoneVolume></FaPhoneVolume>
+                                </div>
+                            </div>
+                            <p className='text-gray-800 text-sm ml-16'>+92-321343241</p>
+                        </div>
+                    </div>
                 </div>
-                <button
-                    className="text-white text-2xl md:hidden"
-                    onClick={handleToggle}
-                >
-                    &#9776;
-                </button>
-                <ul
+                <div
                     className={`${showLinks ? 'block' : 'hidden'
-                        } md:flex flex-col md:flex-row md:items-center`}
+                        } md:hidden flex flex-col  md:items-center px-6 gap-3`}
                 >
-                    <li className="my-2 md:my-0">
-                        <Link
-                            href="#"
-                            className="text-white hover:text-gray-300 md:mx-4 block"
-                        >
-                            Home
-                        </Link>
-                    </li>
-                    <li className="my-2 md:my-0">
-                        <Link
-                            href="#"
-                            className="text-white hover:text-gray-300 md:mx-4 block"
-                        >
-                            About
-                        </Link>
-                    </li>
-                    <li className="my-2 md:my-0">
-                        <Link
-                            href="#"
-                            className="text-white hover:text-gray-300 md:mx-4 block"
-                        >
-                            Services
-                        </Link>
-                    </li>
-                    <li className="my-2 md:my-0">
-                        <Link
-                            href="#"
-                            className="text-white hover:text-gray-300 md:mx-4 block"
-                        >
-                            Contact
-                        </Link>
-                    </li>
-                </ul>
+                    <div className='flex-row flex items-center gap-2'>
+                        <div className='text-yellow-300 p-2 border-yellow-300 rounded-full border'>
+                            <FaHome></FaHome>
+                        </div>
+                        <div className='flex flex-col gap-1'>
+                            <div className='text-yellow-300 font-bold'> Address</div>
+                            <div className='text-gray-800 text-sm'> Example England</div>
+                        </div>
+                    </div>
+                    <div className='flex-row flex items-center gap-2'>
+                        <div className='text-yellow-300 p-2 border-yellow-300 rounded-full border'>
+                            <FaMailBulk></FaMailBulk>
+                        </div>
+                        <div className='flex flex-col gap-1'>
+                            <div className='text-yellow-300 font-bold'>Email Us</div>
+                            <div className='text-gray-800 text-sm'> example@booking.com</div>
+                        </div>
+                    </div>
+                    <div className='flex-row flex items-center gap-2'>
+                        <div className='text-yellow-300 p-2 border-yellow-300 rounded-full border'>
+                            <FiPhone></FiPhone>
+                        </div>
+                        <div className='flex flex-col gap-1'>
+                            {/* <div className='text-yellow-300'> Address</div>
+                            <div className='text-gray-800 text-sm'> Example England</div> */}
+                            <p className='text-gray-800 text-sm font-bold'>+92-321343241</p>
+                        </div>
+                    </div>
+                </div>
             </nav>
-            <ul className="md:flex flex-col md:flex-row md:items-center ">
-                <li className={`my-2 md:my-0 ${isCurrentPage('/') ? 'text-red-500' : ''}`}>
+            <ul className="md:flex hidden flex-row  w-full h-16 items-center gap-3 bg-slate-800 ">
+                <li className={` ${isCurrentPage('/') ? 'text-red-500' : ''}`}>
                     <Link href="/" className="hover:text-gray-300 md:mx-4 block">
                         Home
                     </Link>
                 </li>
-                <li className={`my-2 md:my-0 ${isCurrentPage('/posts') ? 'text-red-500' : ''}`}>
+                <li className={` ${isCurrentPage('/posts') ? 'text-red-500' : ''}`}>
                     <Link href="/posts" className="hover:text-gray-300 md:mx-4 block">
                         Posts
                     </Link>
